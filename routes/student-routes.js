e640c740f75d962231416e74a9afaf3436e71c6a
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-17 14:35:27 -0800

    Authenticated student routes done

---
+++ routes/student-routes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// npm modules
+const Router = require('express').Router;
+
+// app modules
+const Student = require('../models/student.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
+
+// module constants
+const router = module.exports = new Router();
+
+router.get('/students', bearerAuth, (req, res, next) => {
+  Student.findById(req.student._id)
+    .then(student => {
+      delete student.password;
+      res.json(student);
+    })
+    .catch(next);
+});
+
+router.put('/students', bearerAuth, (req, res, next) => {
+  Student.findOneAndUpdate({_id: req.student._id}, req.body, {new: true})
+    .then(student => {
+      delete student.password;
+      res.json(student);
+    })
+    .catch(next);
+});
+
+router.delete('/students', bearerAuth, (req, res, next) => {
+  Student.findByIdAndRemove(req.student._id)
+    .then(() => res.status(204))
+    .catch(next);
+});
