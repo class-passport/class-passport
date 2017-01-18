357ca9019fb64096a08f7b91ddffac7097cf0795
spamalope01 <spamalope01@yahoo.com>
2017-01-17 20:41:39 -0800

    written get, post and put tests

--- tests/test-students.js
+++ tests/test-students.js
@@ -9,18 +9,18 @@ describe('testing student rotues', function(){
   let student;
   let token;
 
-  // before(function(done) {
-  //   console.log('something happening');
-  //   let tmp = new User({username: 'usertestafa', password: 'testpass'});
-  //   tmp.save()
-  //   .then(u => {
-  //     user = u;
-  //     token = u.generateToken();
-  //     console.log(token);
-  //     console.log('token created');
-  //     done();
-  //   });
-  // });
+  before(function(done) {
+    console.log('something happening');
+    let tmp = new User({username: 'usertestafa', password: 'testpass'}); //may have to add more based on teh schema
+    tmp.save()
+    .then(u => {
+      student = u;
+      token = u.generateToken();
+      console.log(token);
+      console.log('token created');
+      done();
+    });
+  });
 
 
 
@@ -38,11 +38,11 @@ describe('testing student rotues', function(){
 
 
 // test POST errors/messages
-  describe('testing POST /users for response 200', function(){
+  describe('testing POST /students functionality', function(){
     //create a student, no auth
-    it('creates a student and gives a 200', function(done){
+    it('creates a student and gives a 200', function(done){ //may not need this test due to the before
       request.post('localhost:3000/signup')
-      .send({make: 'TESTSTUDENT', model:'TESTPASS'})
+      .send({username: 'TESTSTUDENT', password:'TESTPASS'}) //may have to add more
       .end((err, res) => {
         console.log('body', res.body);
         student = res.body;
@@ -64,45 +64,67 @@ describe('testing student rotues', function(){
     });
 
       //prevent a student from adding a course to the cc
-    it('should not allow a student to add a course to the Community College', function(){
+    it('should not allow a student to add a course to the Community College', function(done){
       request.post('localhost:3000/cccourse')
-      .auth('Bearer', '', {type:'auto'})
-      .send({make: 'usertestafa', model:'testpass'})
+      .auth('Bearer', token, {type:'auto'})
+      // .send({username: 'usertestafa', password:'testpass'})  //not sure if i need to send this
       .end((err, res) => {
+        expect(request.role).to.equal('student'); //need to verify how we're defining that
         expect(res.status).to.equal(401);
-        // done();
+        done();
       });
     });
 
     //prevent a student from adding a course that is not offered by the cc
-    // it('should return a 400 error if no body is provided', function(){
-    //   request.post('localhost:3000/users')
-    //   .end((err, res) => {
-    //     expect(res.status).to.equal(400);
-    //   });
-    // });
+    it('should not allow a student to add a course that is not offered by the Community College', function(done){
+      request.post('localhost:3000/student/cccourse')
+      .auth('Bearer', token, {type: 'auto'})
+      // .send({username: 'usertestafa', password:'testpass'})
+      .send({code: 'Basic Shoe Leather Tasting 105'})
+      .end((err, res) => {
+        expect(res.status).to.equal(400); //this will depend on what is written in the routes.
+        done();
+      });
+    });
 
-    //it should return a 401 error if an invalid token is provided for a student
+    it('should return a 401 error if an invalid token is provided for a student', function(done){
+      request.post('localhost:3000/student/cccourse')
+      .auth('Bearer', 'badtoken', {type: 'auto'})
+      // .send({username: 'usertestafa', password:'testpass'})
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
+    });
 
 
-    //it should return a 401 error if no token was provided for a student
+    it('should return a 401 error if no token was provided for a student', function(done){
+      request.get('localhost:3000/student/cccourse')
+      .auth('Bearer', '', {type:'auto'})
+      // .send({make: 'usertestafa', model:'testpass'})
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
+    });
 
-    // it('should return a 400 error if no body is provided in the POST request', function(){
-    //   request.post('localhost:3000/users')
-    //   .end((err, res) => {
-    //     expect(res.status).to.equal(400);
-    //   });
-    // });
+    it('should return a 400 error if no body is provided in the POST request', function(){
+      request.post('localhost:3000/studend/cccourse')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+      });
+    });
   });
 
 
 // testing GET errors/messages
-  describe('testing GET /users respones', function(){
+  describe('testing GET /students functionality', function(){
+
     //return a student's information except password
-    it('provided an id it should return a user but not the password', function(){
-      request.get('localhost:3000/users')
+    it('provided an id it should return a student but not the password', function(){
+      request.get('localhost:3000/student')
       .auth('Bearer', token, {type:'auto'})
-      .send({make: 'usertestafa', model:'testpass'})
+      // .send({make: 'usertestafa', model:'testpass'})
       .end((err, res) => {
         if (err) return (err);
         expect(res.status).to.equal(200);
@@ -112,13 +134,11 @@ describe('testing student rotues', function(){
     });
 
     //return all CC courses
-    it('should return a 401 error if a token was not provided', function(){
-      request.get('localhost:3000/users')
-      .auth('Bearer', '', {type:'auto'})
-      .send({make: 'usertestafa', model:'testpass'})
+    it('should return all courses offered by the Community College for an unauthenticated user', function(done){
+      request.get('localhost:3000/cccourse')
       .end((err, res) => {
-        expect(res.status).to.equal(401);
-        // done();
+        expect(res.body.code).to.not.be.empty;
+        done();
       });
     });
 
@@ -126,7 +146,7 @@ describe('testing student rotues', function(){
     it('should return a 404 error if the id provided was not found', function(){
       request.get('localhost:3000/users')
       .auth('Bearer', 'badToken', {type: 'auto'})
-      .send({make: 'usertestafa', model:'testpass'})
+      // .send({make: 'usertestafa', model:'testpass'})
       .end((err, res) =>{
         expect(res.status).to.equal(404);
       });
@@ -134,49 +154,48 @@ describe('testing student rotues', function(){
   });
 
   // testing PUT errors/messages
-    describe('testing PUT /users respones', function(){
+    describe('testing PUT /student functionality', function(){
       //allow a student to update an existing course
-      it('provided an id it should return a user but not the password', function(){
-        request.get('localhost:3000/users')
+      it('allow a student to update an existing course within their course list', function(){
+        request.get('localhost:3000/student/cccourse')
         .auth('Bearer', token, {type:'auto'})
-        .send({make: 'usertestafa', model:'testpass'})
+        .send({course: 'Making Shoes for Hogs 202'})
         .end((err, res) => {
-          if (err) return (err);
           expect(res.status).to.equal(200);
-          expect(res.body.username).to.equal('usertestafa');
-          expect(res.body).to.not.have.property('password');
         });
       });
 
       //prevent a student from updating a CC course
-      it('should return a 401 error if a token was not provided', function(){
-        request.get('localhost:3000/users')
-        .auth('Bearer', '', {type:'auto'})
-        .send({make: 'usertestafa', model:'testpass'})
+      it('should return a 401 error if a token was not provided', function(done){
+        request.post('localhost:3000/student/cccourse')
+        .auth('Bearer', token, {type: 'auto'})
+        // .send({username: 'usertestafa', password:'testpass'})
+        .send({code: 'Making Shoes for Hogs 202'})
         .end((err, res) => {
-          expect(res.status).to.equal(401);
-          // done();
+          expect(res.status).to.equal(400); //this will depend on what is written in the routes.
+          done();
         });
       });
 
-      // //return all
-      // it('should return a 404 error if the id provided was not found', function(){
-      //   request.get('localhost:3000/users')
-      //   .auth('Bearer', 'badToken', {type: 'auto'})
-      //   .send({make: 'usertestafa', model:'testpass'})
-      //   .end((err, res) =>{
-      //     expect(res.status).to.equal(404);
-      //   });
-      // });
+      //prevent a student from updating another student's courses
+      it('should prevent a student from updating another student\'s courses', function(){
+        request.post('localhost:3000/student/cccourse')
+        .auth('Bearer', )
+
+          //THIS WILL HAVE TO WAIT UNTIL EVERYTHING ELSE IS DONE BUT BASICALLY WE'RE GOING TO LET BOB LOG IN AS BOB AND THEN HAVE HIM CHANGE SOMETHING...OR TRY TO TO CHANGE SALLY'S COURSES (LIKE SAY WE WANT TO UPDATE RECORD ID 6), THEN ENSURE THAT NOT ONLY DID HE FAIL, BUT RECORD ID 6 IS THE SAME AS IT WAS BEFORE.
+      })
+
+
     });
 
     // testing DELETE errors/messages
-      describe('testing DELETE /users respones', function(){
+      describe('testing DELETE /students functionality', function(){
         //allow a student to delete a course from their course listing
-        it('provided an id it should return a user but not the password', function(){
-          request.get('localhost:3000/users')
+        it('should allow a student to delete a course from their course listing', function(){
+          request.get('localhost:3000/student/cccourse')
           .auth('Bearer', token, {type:'auto'})
           .send({make: 'usertestafa', model:'testpass'})
+          //student.deleteOne(coursename)
           .end((err, res) => {
             if (err) return (err);
             expect(res.status).to.equal(200);
