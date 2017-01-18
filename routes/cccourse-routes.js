1bfc682ccff0723d3ba1cb7481fdb5a9db30edc9
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-18 11:27:43 -0800

    admin funtionality is working

--- routes/cccourse-routes.js
+++ routes/cccourse-routes.js
@@ -1,11 +1,13 @@
 'use strict';
 
-let Router = require('express').Router;
+// npm modules
+const Router = require('express').Router;
 
-let CCCourse = require('../models/cccourse.js');
-let bearerAuth = require('../lib/bearer-auth-middleware.js');
+// app modules
+const CCCourse = require('../models/cccourse.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
-
+// module constants
 const router = module.exports = new Router();
 
 router.post('/cccourses', bearerAuth, (req, res, next) => {
@@ -18,41 +20,47 @@ router.post('/cccourses', bearerAuth, (req, res, next) => {
   // ADMIN: creates new course
   const course = new CCCourse(req.body);
   course.save()
-  .then(course => res.json(course))
-  .catch(next);
+    .then(course => res.json(course))
+    .catch(next);
 });
 
 router.get('/cccourses', bearerAuth, (req, res, next) => {
   // STUDENT and ADMIN: returns all courses for both student and admin(if used)
   CCCourse.find({})
-  .then(courses => res.json(courses))
-  .catch(next);
+    .then(courses => res.json(courses))
+    .catch(next);
+});
+
+router.get('/cccourses/:id', bearerAuth, (req, res, next) => {
+  // STUDENT and ADMIN: returns one course for both student and admin(if used)
+  CCCourse.findById(req.params.id)
+    .then(course => res.json(course))
+    .catch(next);
 });
 
 router.put('/cccourses/:id', bearerAuth, (req, res, next) => {
   //ADMIN: ideally only admins would be able to update a course
-  CCCourse.findByIdAndUpdate(req.params._id, req.body, {new:true})
-  .then(course => res.json(course))
-  .catch(next);
+  CCCourse.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .then(course => res.json(course))
+    .catch(next);
 });
-
 
 router.delete('/cccourses/:id', bearerAuth, function(req, res, next) {
   //STUDENT: deletes ($pulls) a selected course from curr_courses by id
 
   //ADMIN: ideally only an admin would be able to remove a course from collection
   CCCourse.findByIdAndRemove(req.params.id)
-  .then(() => res.status(204).end())
-  .catch(next);
+    .then(() => res.status(204).end())
+    .catch(next);
 });
 
 router.get('/cccourses', (req, res) => {
   CCCourse.find({})
-  .then(courses => {
-    res.write('list of community college courses:' + '\n');
-    courses.forEach(function(course) {
-      res.write(course.code + '\n');
+    .then(courses => {
+      res.write('list of community college courses:' + '\n');
+      courses.forEach(function(course) {
+        res.write(course.code + '\n');
+      });
+      res.end();
     });
-    res.end();
-  });
 });
