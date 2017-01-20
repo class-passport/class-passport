50dfeb66338e63dcadfb26cbd65bfc33edfbc13a
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-01-19 18:54:46 -0800

    200 test passing

--- test/test-auth.js
+++ test/test-auth.js
@@ -26,65 +26,118 @@ describe('Testing Auth Routes', function() {
     server = app.listen(PORT, () => console.log('started tests from user tests'));
     done();
   });
+//
+//   describe('testing POST /signup routes', function() {
+//     after((done) => {
+//       User.remove({}).exec();
+//       done();
+//     });
+//
+//     it('should return 200 and token on student signup', (done) => {
+//       request.post('localhost:3000/signup')
+//       .send(exampleStudent)
+//       .end((err, res) => {
+//         expect(res.status).to.equal(200);
+//         expect(res.body).to.be.a('string');
+//         done();
+//       });
+//     });
+//
+//     it('should return 200 and token on admin signup', (done) => {
+//       request.post('localhost:3000/signup')
+//       .send(exampleAdmin)
+//       .end((err, res) => {
+//         expect(res.status).to.equal(200);
+//         expect(res.body).to.be.a('string');
+//         done();
+//       });
+//     });
+//
+//     it('should return 400 with no body provided', (done) => {
+//       request.post('localhost:3000/signup')
+//       .send()
+//       .end((err, res) => {
+//         expect(res.status).to.equal(400);
+//         done();
+//       });
+//     });
+//
+//     it('should return 400 with invalid body provided', (done) => {
+//       request.post('localhost:3000/signup')
+//       .send('Invalid Body')
+//       .end((err, res) => {
+//         expect(res.status).to.equal(400);
+//         done();
+//       });
+//     });
+//
+//     it('should return 409 if username is already in database', (done) => {
+//       request.post('localhost:3000/signup')
+//       .send(exampleStudent)
+//       .end((err, res) => {
+//         expect(res.status).to.equal(409);
+//         expect(res.text).to.equal('ConflictError');
+//         done();
+//       });
+//     });
+//   });
+//
+//   after((done) => {
+//     server.close(() => console.log('server closed after user tests'));
+//     done();
+//   });
+// });
 
-  describe('testing POST /signup routes', function() {
-    after((done) => {
-      User.remove({}).exec();
+describe('testing GET /login route', function(){
+
+  before(done => {
+    let user = new User(exampleStudent);
+    user.hashPassword(exampleStudent.password)
+      .then(user => user.save())
+      .then(user => {
+        this.tempUser = user;
+        done();
+      })
+      .catch(done);
+  });
+
+  after(done => {
+    User.remove({})
+      .then(() => done())
+      .catch(done);
+  });
+
+  it('should return a token', function(done){
+    request.get('localhost:3000/login')
+    .auth('exampleStudent', '1234')
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.a('string');
       done();
     });
-
-    it('should return 200 and token on student signup', (done) => {
-      request.post('localhost:3000/signup')
-      .send(exampleStudent)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.a('string');
-        done();
-      });
-    });
-
-    it('should return 200 and token on admin signup', (done) => {
-      request.post('localhost:3000/signup')
-      .send(exampleAdmin)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.a('string');
-        done();
-      });
-    });
-
-    it('should return 400 with no body provided', (done) => {
-      request.post('localhost:3000/signup')
-      .send()
-      .end((err, res) => {
-        expect(res.status).to.equal(400);
-        done();
-      });
-    });
-
-    it('should return 400 with invalid body provided', (done) => {
-      request.post('localhost:3000/signup')
-      .send('Invalid Body')
-      .end((err, res) => {
-        expect(res.status).to.equal(400);
-        done();
-      });
-    });
-
-    it('should return 409 if username is already in database', (done) => {
-      request.post('localhost:3000/signup')
-      .send(exampleStudent)
-      .end((err, res) => {
-        expect(res.status).to.equal(409);
-        expect(res.text).to.equal('ConflictError');
-        done();
-      });
-    });
   });
 
-  after((done) => {
-    server.close(() => console.log('server closed after user tests'));
-    done();
-  });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 });
