ef66dfa9082b2e463afe591147bcd7991641e0a3
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-01-22 01:26:49 -0800

    Trying to get travis to work

--- routes/uwcourse-routes.js
+++ routes/uwcourse-routes.js
@@ -1,5 +1,4 @@
 let Course = require('../models/uwcourse');
-let jsonParser = require('body-parser').json();
 let createError = require('http-errors');
 let bearerAuth = require('../lib/bearer-auth-middleware');
 
@@ -25,30 +24,29 @@ router.get('/uwcourse', (req, res) => {
   Course.find({}).then(courses => res.json(courses));
 });
 
-
-  router.put('/users', bearerAuth, (req, res) => {
-    if(req.user.admin) {
-      User.update((req.body), function(err){
-        if(err) {
-          res.status(400).end('bad request');
-        } else {
-          res.status(200).json({msg: 'updated the user'})
-        }
-      });
-    } else{
-      if(err) {
-        console.log(err);
-        res.status(404).end('not found');
-      }
-    }
-  });
-
-  router.delete('/users', bearerAuth, (req, res) => {
-    if(req.user) {
-      delete req.user
-      .then( () => res.status(204).send())
-      .catch(err => createError(404, 'Not Found'));
-    } else {
-      res.json({msg: 'you are not authorized to delete'})
-    }
-  });
+  // router.put('/users', bearerAuth, (req, res) => {
+  //   if(req.user.admin) {
+  //     User.update((req.body), function(err){
+  //       if(err) {
+  //         res.status(400).end('bad request');
+  //       } else {
+  //         res.status(200).json({msg: 'updated the user'})
+  //       }
+  //     });
+  //   } else{
+  //     if(err) {
+  //       console.log(err);
+  //       res.status(404).end('not found');
+  //     }
+  //   }
+  // });
+  //
+  // router.delete('/users', bearerAuth, (req, res) => {
+  //   if(req.user) {
+  //     delete req.user
+  //     .then( () => res.status(204).send())
+  //     .catch(err => createError(404, 'Not Found'));
+  //   } else {
+  //     res.json({msg: 'you are not authorized to delete'})
+  //   }
+  // });
