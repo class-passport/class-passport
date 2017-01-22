2129e15aed003789314d869f1167d4c007260efe
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-01-22 13:46:03 -0800

    tests for student route now working properly

--- test/test-students.js
+++ test/test-students.js
@@ -1,65 +1,95 @@
-'use strict';
+let request = require('superagent');
+let expect = require('chai').expect;
+let app = require('../index.js');
+let User = require('../models/user');
+let Courses = require('../models/cccourse');
+let PORT = process.env.PORT || 3000;
 
-const request = require('superagent');
-const expect = require('chai').expect;
-
-const User = require('../models/user.js');
-const app = require('../index.js');
-const PORT = process.env.PORT || 3000;
-
-const exampleStudent = {
-  username: 'exampleStudent',
-  password: '1234',
-  admin: false
-};
-
-const exampleAdmin = {
-  username: 'exampleAdmin',
-  password: '1234',
-  admin: true
-};
-
-describe('Testing Student Routes', () => {
+describe('testing cccourse routes', function(){
   let server;
+  let student;
+  let token;
+  let course;
+  let badToken = 'adfawr234q2345234';
+  let courseID;
+  let admin;
+  let adminToken;
 
-  before((done) => {
-    server = app.listen(PORT, () => console.log('started tests from user tests'));
+  before(function(done) {
+    server = app.listen(PORT, () => console.log('started tests from cccourse tests'));
+
+    let tempStudent = new User({username: 'mars', password: '1234', admin: false});
+    let tempAdmin = new User({username: 'perry', password: '4321', admin: true});
+    tempAdmin.save()
+    .then(userAdmin => {
+      admin = userAdmin;
+      userAdmin.generateToken()
+      .then(tokenA => {
+        adminToken = tokenA;
+      });
+    });
+    tempStudent.save()
+    .then(userStudent => {
+      student = userStudent;
+      userStudent.generateToken()
+      .then(tokenS => {
+        token = tokenS;
+      });
+    });
+    let cmp = new CC({code: 'Eng 101'});
+    cmp.save()
+    .then(c => {
+      courseID = c._id;
+      course = c;
+      done();
+    });
+  });
+
+  after(function(done) {
+    User.remove({_id:student._id}).exec();
+    User.remove({_id:admin._id}).exec();
+    CC.remove({_id:course._id}).exec();
+
+    server.close(() => console.log('server closed after cccourse tests'));
     done();
   });
 
-  describe('testing get /students routes', () => {
-    after(done => {
-      User.remove({})
-        .then(() => done())
-        .catch(done);
+  //Unregistered route
+  describe('testing unregistered route', function(){
+    it('should return 404 for an unregistered route', function(done) {
+      request.get('localhost:3000/cats')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        done();
+      });
     });
   });
+  //
+  describe('testing GET /students route', function(){
 
-  it('should return 401 if the user is not authorized', (done) => {
-    request.get('localhost:3000/students')
-    .auth('exampleStudent', '4321')
-    .end((err, res) => {
-      expect(res.status).to.equal(401);
-      done();
+    it('should allow a student to access /students route', function(done){
+      request.get('localhost:3000/students')
+      .set('Authorization', 'Bearer ' + token)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
     });
-  });
 
-  it('should return an error if an admin is accessing this route', (done) => {
-    request.get('localhost:3000/students')
-    .send(exampleAdmin)
-    .end((err, res) => {
-      expect(res.status).to.equal(401);
-      done();
+    it('should not allow an admin to access /students route', function(done){
+      request.get('localhost:3000/students')
+      .set('Authorization', 'Bearer ' + adminToken)
+      .set('Accept', 'application/json')
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
     });
+
+
+
+
+
   });
-
-
-
-
-
-
-
-
-
-
 });
