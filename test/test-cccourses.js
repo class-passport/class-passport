84380cfb05fb14ecf76c922176b72fea19a122b6
Jessica Soltero <jmichellevs@gmail.com>
2017-01-27 11:18:52 -0800

    Removed users password from response, returned 204 on delete

--- test/test-cccourses.js
+++ test/test-cccourses.js
@@ -148,7 +148,7 @@ describe('testing cccourse routes', function(){
       request.get('localhost:3000/cccourses')
       .end((err, res) => {
         expect(res.status).to.equal(200);
-        expect(res.text).to.contain('Eng 101');
+        expect(res.body[0].code).to.equal('Eng 101');
         done();
       });
     });
@@ -225,7 +225,8 @@ describe('testing cccourse routes', function(){
       request.delete('localhost:3000/cccourses/' + courseID)
       .set('Authorization', 'Bearer ' + token)
       .end((err, res) => {
-        expect(res.body.curr_courses).to.not.contain(courseID);
+        expect(res.status).to.equal(204);
+        expect(res.body).to.deep.equal({});
         done();
       });
     });
